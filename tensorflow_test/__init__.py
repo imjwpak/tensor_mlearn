9f73defc94e5e980f5e8cfa41aa411e5c6935d04
imjwpak83 <imjwpak83@naver.com>
2019-09-07 17:22:03 +0900

    20190907 17:21

--- tensorflow_test/__init__.py
+++ tensorflow_test/__init__.py
@@ -1,8 +1,19 @@
 from tensorflow_test.mammal import Mammal
 from tensorflow_test.word_sequence import WordSequence
+from tensorflow_test.naive_bayes import NaiveBayes
+from tensorflow_test.web_crawler import WebCrawler
 
 if __name__ == '__main__':
     #mm = Mammal()
     #Mammal.execute()
 
-    WordSequence.execute()
+    #WordSequence.execute()
+
+    #t = WebCrawler.create_model()
+    nb = NaiveBayes()
+    nb.train('./data/review_train.csv')
+    #print(nb.classify('내 인생에서 쓰레기 같은 영화'))
+    print(nb.classify('내 인생에서 최고의 영화'))
+
+
+
--- tensorflow_test/web_crawler.py
+++ tensorflow_test/web_crawler.py
@@ -1,5 +1,25 @@
+from selenium import webdriver
+from time import sleep
+from bs4 import BeautifulSoup
+
 class WebCrawler:
     def __init__(self):
         pass
+
+    @staticmethod
+    def create_model():
+        context = './data/'
+        driver = webdriver.Chrome(context + 'chromedriver')
+        driver.get('https://movie.naver.com/movie/bi/mi/review.nhn?code=179158')
+
+        soup = BeautifulSoup(driver.page_source, 'html.parser')
+        all_divs = soup.find_all('ul', attrs={'class', 'rvw_list_area'})
+        products = [ul.li.p.a.string for ul in all_divs]
+
+        # reviewTab > div > div > ul > li:nth-child(1) > p > a
+        driver.close()
+        print('movies : {}'.format(products))
+
+        return products
 
 
